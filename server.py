318fb5e5ba5d9fa3be77abf21f5268ae4066d934
ramakrishna <ramakrishnachavali15@gmail.com>
2023-11-14 01:51:27 +0530

    commit to add files from locally

---
+++ server.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+messages = [ "Subscribe", "Like", "Comment", "Eat a cookie" ]
+@app.route('/')
+def index():
+    message = messages [random.randint(0, 3)]
+    return render_template('index.html', message=message)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__': 
+    app.run(debug=True) 
